0c0eda4eb31b2fe12ae2938f705b5e736545cb81
Plamen Tsanov <plamen.tsanov@atia.com>
2020-01-15 16:18:17 +0200

    Test commit

--- src/main/java/com/myshop/MyShopApplication.java
+++ src/main/java/com/myshop/MyShopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyshopApplication.class, args);
+        SpringApplication.run(MyShopApplication.class, args);
     }
-
+    //Bachka
 }
